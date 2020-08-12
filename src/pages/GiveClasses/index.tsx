42022378c6fbdcb2d41fbb9f3087539a66ee89f7
burn-c <chopcsom@gmail.com>
2020-08-11 22:59:00 -0300

    Feat: GiveClasses page

--- src/pages/GiveClasses/index.tsx
+++ src/pages/GiveClasses/index.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import { View } from 'react-native';
 
-// import { Container } from './styles';
+import giveClassesBgImage from '../../assets/images/give-classes-background.png';
+
+import { Container, Content, Title, Description, OkButton, OkButtonText } from './styles';
+import { useNavigation } from '@react-navigation/native';
+
 
 const GiveClasses: React.FC = () => {
-  return <View />;
+  const { goBack } = useNavigation();
+
+  function handleNavigateBack() {
+    goBack();
+  }
+
+
+  return (
+    <Container>
+      <Content resizeMode="contain" source={giveClassesBgImage}>
+        <Title>Quer ser um Proffy?</Title>
+        <Description>Para começar, você precisa se cadastrar como professor na nossa plataforma web.</Description>
+      </Content>
+      <OkButton onPress={handleNavigateBack}>
+        <OkButtonText>Tudo bem</OkButtonText>
+      </OkButton>
+
+    </Container>
+  );
 }
 
 export default GiveClasses;
